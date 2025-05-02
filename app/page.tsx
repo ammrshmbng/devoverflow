2d2e6f5734e1fbd6c41705ba5883108280e357d9
ammar sihombing <ammarmare01@gmail.com>
2025-05-02 15:13:42 +0700

    server component vs client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Hello from "./components/hello";
 
 export default function Home() {
+  
+  console.log("what am i doing here CLIENT/SERVER")
+
+
   return (
-   <div>
+   <>
       <h1 className="text-3xl font-bold underline">welcome nextjs</h1>
-   </div>
+      <Hello/>
+   </>
   );
 }
