1f4d5ed908059a495d7b22ce177aaf7626bdb88d
ammar sihombing <ammarmare01@gmail.com>
2025-05-06 16:03:59 +0700

    implement simple logout functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 export default async function Home() {
   const sesstion = await auth();
@@ -8,6 +10,16 @@ export default async function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">welcome nextjs</h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Logout</Button>
+      </form>
     </>
   );
 }
