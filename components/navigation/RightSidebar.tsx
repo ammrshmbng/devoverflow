e089261d22da5ff9878b9db0ec23286393de3ac6
ammar sihombing <ammarmare01@gmail.com>
2025-05-07 13:45:52 +0700

    create TagCard component for popular tags

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -4,12 +4,22 @@ import React from "react";
 
 import ROUTES from "@/constants/routes";
 
+import TagCard from "../cards/TagCard";
+
 const hotQuestions = [
   { _id: "1", title: "How to create a custom hook in React?" },
   { _id: "2", title: "How to use React Query?" },
   { _id: "3", title: "How to use Redux?" },
   { _id: "4", title: "How to use React Router?" },
   { _id: "5", title: "How to use React Context?" },
+];
+
+const popularTags = [
+  { _id: "1", name: "react", questions: 100 },
+  { _id: "2", name: "javascript", questions: 200 },
+  { _id: "3", name: "typescript", questions: 150 },
+  { _id: "4", name: "nextjs", questions: 50 },
+  { _id: "5", name: "react-query", questions: 75 },
 ];
 
 const RightSidebar = async () => {
@@ -39,6 +49,18 @@ const RightSidebar = async () => {
 
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map(({ _id, name, questions }) => (
+            <TagCard
+              key={_id}
+              _id={_id}
+              name={name}
+              questions={questions}
+              showCount
+              compact
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
