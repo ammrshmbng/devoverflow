c127f6cde661b0fbe60e075cd6419d77363d1586
ammar sihombing <ammarmare01@gmail.com>
2025-05-07 17:37:27 +0700

    create LocalSearch component
    and install query-string package
    
    render static data and search and query them

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,49 @@
 import Link from "next/link";
 
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 
-export default async function Home() {
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn Next.js?",
+    description: "I want to learn Next.js, can anyone help me?",
+    tags: [
+      { _id: "1", name: "Next.js" },
+      { _id: "2", name: "React" },
+    ],
+    author: { _id: "2", name: "Jane Smith" },
+    upvotes: 15,
+    answers: 3,
+    views: 150,
+    createdAt: new Date(),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+export default async function Home({ searchParams }: SearchParams) {
+  const { query = "" } = await searchParams;
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -18,14 +58,21 @@ export default async function Home() {
         </Button>
       </section>
       <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        Local Search
+        <LocalSearch
+          route={ROUTES.HOME}
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          iconPosition="left"
+          otherClasses="flex-1"
+        />
       </section>
-      HomeFilter
+      {/* HomeFilter */}
       <div className="mt-10 flex w-full flex-col gap-6">
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
+        <div className="mt-10 flex w-full flex-col gap-6">
+          {filteredQuestions.map((question) => (
+            <h1 key={question._id}>{question.title}</h1>
+          ))}
+        </div>
       </div>
     </>
   );
