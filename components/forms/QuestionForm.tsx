cc94a830510feee411ea65c1bddeaf4fe690d2fa
ammar sihombing <ammarmare01@gmail.com>
2025-05-08 15:17:13 +0700

    install package cm6-theme-basic-dark
    create mdxeditor component
    
    complete quetions form

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
 
 import { AskQuestionSchema } from "@/lib/validations";
@@ -19,7 +21,17 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 
-const QuestionForm = () => {
+const Editor = dynamic(() => import("@/components/editor"), {
+  ssr: false,
+});
+
+interface Params {
+  question?: Question;
+  isEdit?: boolean;
+}
+
+const QuestionForm = ({ question, isEdit = false }: Params) => {
+  const editorRef = useRef<MDXEditorMethods>(null);
   const form = useForm({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -78,11 +90,11 @@ const QuestionForm = () => {
                 <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl>
-                {/* <Editor
+                <Editor
                   value={field.value}
                   editorRef={editorRef}
                   fieldChange={field.onChange}
-                /> */}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you&apos;ve put in the
