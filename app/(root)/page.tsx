4db085b82b4471dc594019754911982bf270bb6a
ammar sihombing <ammarmare01@gmail.com>
2025-05-13 16:26:20 +0700

    implementing pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,12 @@ import QuestionCard from "@/components/cards/QuestionCard";
 import DataRenderer from "@/components/DataRenderer";
 import CommonFilter from "@/components/filters/CommonFilter";
 import HomeFilter from "@/components/filters/HomeFilter";
+import Pagination from "@/components/Pagination";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import ROUTES from "@/constants/routes";
 import { EMPTY_QUESTION } from "@/constants/states";
-// import Pagination from "@/components/Pagination";
 import { getQuestions } from "@/lib/actions/question.action";
 
 export const metadata: Metadata = {
@@ -77,7 +77,7 @@ async function Home({ searchParams }: RouteParams) {
         )}
       />
 
-      {/* <Pagination page={page} isNext={isNext || false} /> */}
+      <Pagination page={page} isNext={isNext || false} />
     </>
   );
 }
