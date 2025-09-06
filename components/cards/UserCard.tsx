5415b9c3ac04a4d6b8dbddf44bdeb5849a23318e
ammar sihombing <ammarmare01@gmail.com>
2025-09-06 10:49:38 +0700

    adjust background color

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -6,7 +6,7 @@ import UserAvatar from "../UserAvatar";
 
 const UserCard = ({ _id, name, image, username }: User) => (
   <div className="shadow-light100_darknone w-full xs:w-[230px]">
-    <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
+    <article className="background-light800_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
       <UserAvatar
         id={_id}
         name={name}
@@ -17,7 +17,7 @@ const UserCard = ({ _id, name, image, username }: User) => (
 
       <Link href={ROUTES.PROFILE(_id)}>
         <div className="mt-4 text-center">
-          <h3 className="h3-bold text-dark200_light900 line-clamp-1">{name}</h3>
+          <h3 className="h3-bold text-dark400_light900 line-clamp-1">{name}</h3>
           <p className="body-regular text-dark500_light500 mt-2">@{username}</p>
         </div>
       </Link>
