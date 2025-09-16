98d460f4f34bdb3bb46e6f951d803e0f11c29ff7
ammar sihombing <ammarmare01@gmail.com>
2025-09-16 16:16:07 +0700

    add z-index

--- components/answers/AllAnswers.tsx
+++ components/answers/AllAnswers.tsx
@@ -22,7 +22,7 @@ const AllAnswers = ({
 }: Props) => {
   return (
     <div className="mt-11">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between relative z-10">
         <h3 className="primary-text-gradient">
           {totalAnswers} {totalAnswers === 1 ? "Answer" : "Answers"}
         </h3>
